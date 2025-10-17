85552fbf16a1f200e63d10e9717af26c23c246cc
thatsarealstar <thatsarealstar@gmail.com>
2025-10-17 12:10:10 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,16 @@
-// This is the command designed for testing. You run it, then it sends a simple message.
-let msgobjurl = "https://raw.githubusercontent.com/thatsarealstar/SherlockBot/refs/heads/main/msg.js"
+// The command designed for testing!
+let msgobjurl = "https://raw.githubusercontent.com/thatsarealstar/SherlockBot/refs/heads/main/msg.js";
+
 let response = await fetch(msgobjurl);
 let script = await response.text();
 
-eval(script)
+// Execute the msg.js to make msgobj global
+eval(script);
 
-MPP.client.sendArray([{m: "a", message: "Success! Commands are functional.", reply_to: msgobj}])
-console.log("[LOG] Commands status functional!")
+// Now that msgobj is global, we can use it
+if (window.msgobj) {
+  MPP.client.sendArray([{ m: "a", message: "Success! Commands are functional.", reply_to: window.msgobj.id }]);
+  console.log("[INFO] Commands status functional!");
+} else {
+  console.log("[ERROR] msgobj is not available.");
+}
