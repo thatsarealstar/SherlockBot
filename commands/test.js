6b5b085b69ce4581fd66b538f196b51aa1aa4639
thatsarealstar <thatsarealstar@gmail.com>
2025-10-17 12:05:20 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,3 +1,9 @@
 // This is the command designed for testing. You run it, then it sends a simple message.
-MPP.client.sendArray([{m: "a", message: "Success! Commands are functional.", reply_to: msgid}])
+let msgobjurl = "https://raw.githubusercontent.com/thatsarealstar/SherlockBot/refs/heads/main/msg.js"
+let response = await fetch(msgobjurl);
+let script = await response.text();
+
+eval(script)
+
+MPP.client.sendArray([{m: "a", message: "Success! Commands are functional.", reply_to: msgobj}])
 console.log("[LOG] Commands status functional!")
